1b3297a88478d7f06aeb2c6ddf4289b6fae5aeeb
fofosharawi <yazansharawi25@gmail.com>
2022-09-20 08:05:48 +0300

    New

---
+++ lib/Main/Screens/SignIn.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class signin extends StatefulWidget {
+  @override
+  _signinState createState() => _signinState();
+}
+
+class _signinState extends State<signin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+}
+
